b75eb6baafcaeeaaf80ce0af588d4631bc15d0dd
Primalivet <gustafholm1@gmail.com>
2018-02-09 15:50:57 +0100

    add signup route

--- frontend/mytrip-login/server/routes/index.js
+++ frontend/mytrip-login/server/routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const sessionsController = require('../controllers/sessionsController');
 
 router.post('/login', sessionsController.create);
-router.post('/register', sessionsController.register);
+router.post('/signup', sessionsController.register);
 
 module.exports = router;
