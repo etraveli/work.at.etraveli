f3235e6fd1a41cff9432fb2178d8e24ec5ca3eed
Primalivet <gustafholm1@gmail.com>
2018-02-09 10:51:15 +0100

    prepare mytripapi for register register method

--- frontend/mytrip-login/src/services/myTripAPI.js
+++ frontend/mytrip-login/src/services/myTripAPI.js
@@ -1,19 +1,21 @@
+import crypto from 'crypto-browserify';
 import { Left, Right } from '../utils/generalUtils';
+import _encryption from '../utils/encryption';
+
+const encryption = _encryption(crypto);
 
 const defaultConfig = {
   credentials: 'same-origin',
   headers: {
-    'Content-Type': 'application/json',
-  },
+    'Content-Type': 'application/json'
+  }
 };
 
 const successfulResponse = response =>
-  (response.status >= 200 && response.status < 300);
+  response.status >= 200 && response.status < 300;
 
 const eitherSuccessOrFail = response =>
-  successfulResponse(response)
-    ? Right(response)
-    : Left(response);
+  successfulResponse(response) ? Right(response) : Left(response);
 
 const handleError = response => {
   const error = new Error(`HTTP Error ${response.statusText}`);
@@ -23,20 +25,36 @@ const handleError = response => {
 };
 
 const checkStatus = response =>
-  eitherSuccessOrFail(response)
-    .fold(
-      () => handleError(response),
-      () => response
-    );
+  eitherSuccessOrFail(response).fold(
+    () => handleError(response),
+    () => response
+  );
 
-const parseJSON = response =>
-  response.json();
+const parseJSON = response => response.json();
 
 export const login = (email, bookingNumber) =>
-  fetch('/login',
+  fetch(
+    '/login',
     Object.assign({}, defaultConfig, {
       method: 'POST',
-      body: JSON.stringify({ email, bookingNumber }),
+      body: JSON.stringify({
+        email,
+        bookingNumber: encryption.encrypt(email, bookingNumber)
+      })
     })
-  ).then(checkStatus)
-   .then(parseJSON);
+  )
+    .then(checkStatus)
+    .then(parseJSON);
+
+export const register = email =>
+  fetch(
+    '/register',
+    Object.assign({}, defaultConfig, {
+      method: 'POST',
+      body: JSON.stringify({
+        email
+      })
+    })
+  )
+    .then(checkStatus)
+    .then(parseJSON);
