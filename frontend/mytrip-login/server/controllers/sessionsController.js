a86d5cac5bdc8a24d4894450a1b9a1bbd8614e00
Primalivet <gustafholm1@gmail.com>
2018-02-09 10:51:37 +0100

    prepare server for register route

--- frontend/mytrip-login/server/controllers/sessionsController.js
+++ frontend/mytrip-login/server/controllers/sessionsController.js
@@ -2,6 +2,7 @@ const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const Left = require('../../src/utils/generalUtils').Left;
 const Right = require('../../src/utils/generalUtils').Right;
+const encryption = require('../../src/utils/encryption')(crypto);
 
 const secret = 'HowardHughes';
 
@@ -11,27 +12,51 @@ const createHmac = data =>
     .update(data)
     .digest('hex');
 
-const createToken = data =>
-  jwt.sign({ data }, createHmac(data));
+const createToken = data => jwt.sign({ data }, createHmac(data));
 
-const isEmpty = str =>
-  (str === '' || str === undefined || str === null);
+const isEmpty = str => str === '' || str === undefined || str === null;
 
 const authenticateCustomer = (email, bookingNumber) =>
-  (email === 'invalid' || isEmpty(email) || isEmpty(bookingNumber))
+  email === 'invalid' || isEmpty(email) || isEmpty(bookingNumber)
     ? Left('Unauthorized login attempt')
     : Right(createToken(email));
 
+const generateBookingNumber = email => {
+  try {
+    const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const NUMS = '0123456789';
+    const bookingNumber = new Array(6)
+      .map(
+        i =>
+          i < 4
+            ? NUMS.charAt(Math.floor(Math.random() * NUMS.lenght))
+            : CHARS.charAt(Math.floor(Math.random() * CHARS.lenght))
+      )
+      .join('');
+    console.log(bookingNumber);
+    return Right({ email, bookingNumber });
+  } catch (e) {
+    return Left('Unable to generate new booking number');
+  }
+};
+
 module.exports = {
   create(request, response) {
-    const {
-      email,
-      bookingNumber,
-    } = request.body;
-    authenticateCustomer(email, bookingNumber)
-      .fold(
-        message => response.status(401).json({ message }),
-        token => response.status(200).json({ jwt: token })
-      );
+    const { email, bookingNumber } = request.body;
+    const decryptedBookingNumber = encryption.decrypt(email, bookingNumber);
+    authenticateCustomer(email, decryptedBookingNumber).fold(
+      message => response.status(401).json({ message }),
+      token => {
+        response.status(200).json({ jwt: token });
+      }
+    );
+  },
+  register(request, response) {
+    const { email } = request.body;
+    generateBookingNumber(email).fold(
+      message => response.status(400).json({ message }),
+      ({ email, bookingNumber }) =>
+        response.status(200).json({ email, bookingNumber })
+    );
   }
 };
--- frontend/mytrip-login/server/routes/index.js
+++ frontend/mytrip-login/server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const sessionsController = require('../controllers/sessionsController');
 
-router
-  .post('/login', sessionsController.create);
+router.post('/login', sessionsController.create);
+router.post('/register', sessionsController.register);
 
 module.exports = router;
